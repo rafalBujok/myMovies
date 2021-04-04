a0d373c8c8c41f6a3cb8273d5c4f405c15ae99c7
Rafał Bujok <rafal.bujok@gmail.com>
2021-04-05 01:44:50 +0200

    add footer content

--- src/app/components/grid/footer/footer.component.ts
+++ src/app/components/grid/footer/footer.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
-
+  today = new Date;
+  year: number = 0;
   ngOnInit(): void {
+    this.year = this.today.getFullYear()
   }
 
 }
